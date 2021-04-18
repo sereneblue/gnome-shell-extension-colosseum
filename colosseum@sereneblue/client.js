2f7bf77162979275aa9ca5f7ae8b2f90eb63e97e
sereneblue <sereneblue@users.noreply.github.com>
2021-04-18 17:50:10 -0400

    feat: add live scores

---
+++ colosseum@sereneblue/client.js
@@ -0,0 +1,178 @@
+const Soup = imports.gi.Soup;
+
+const STATUS = {
+	SCHEDULED: "1",
+	IN_PROGRESS: "2",
+	FINAL: "3",
+	POSTPONED: "6",
+	END_PERIOD: "22"
+};
+
+var ColosseumClient = class ColosseumClient {
+	constructor(constants, settings) {
+		this.session = new Soup.Session();
+		this.dateFmt = new Intl.DateTimeFormat("en", {
+			month:'2-digit',day:'2-digit', year:'numeric',
+			timeZone: 'Pacific/Pago_Pago'
+		}); 
+		this.timeFmt = new Intl.DateTimeFormat("en", {
+			timeStyle: "short", hour: 'numeric', minute: 'numeric'
+		}); 
+
+		this.BASE_API_URL = 'https://site.api.espn.com/apis/site/v2/sports/';
+		this.API_URLS = {
+			MLB: this.BASE_API_URL + 'baseball/mlb/scoreboard',
+			NBA: this.BASE_API_URL + 'basketball/nba/scoreboard',
+			NFL: this.BASE_API_URL + 'football/nfl/scoreboard',
+			NHL: this.BASE_API_URL + 'hockey/nhl/scoreboard',
+			WNBA: this.BASE_API_URL + 'basketball/wnba/scoreboard'
+		}
+
+		this._CONSTANTS = constants;
+		this._leagues = Object.keys(this._CONSTANTS.PREF_LEAGUES);
+		this._settings = settings;
+	}
+
+	getLeagueScoreboard(league, date, cacheBuster) {
+		let url = `${this.API_URLS[league]}?limit=1000&dates=${date}&${cacheBuster}`;
+        let message = Soup.Message.new('GET', url);
+
+	    return new Promise((resolve, reject) => {
+	        this.session.queue_message(message, () => {
+	            try {
+	                if (message.status_code === Soup.KnownStatusCode.OK) {
+	                    let result = JSON.parse(message.response_body.data);
+
+	                    resolve(result);
+	                } else {
+	                    reject(new Error(message.status_code.toString()));
+	                }
+	            } catch (e) {
+	                reject(e);
+	            }
+	        });
+	    });
+	}
+
+	async getScores() {
+		let scoreboardDate = new Date();
+		let events = [];
+
+		let leagues = this.getEnabledLeagues();
+		let followOnlyMode = this.isFollowOnlyEnabled();
+
+		for (let i = 0; i < leagues.length; i++) {
+			let l = {
+				league: leagues[i],
+				games: [],
+				following: []
+			};
+
+			let followedTeams = this.getFollowedTeams(leagues[i]);
+
+			try {
+				let data = await this.getLeagueScoreboard(
+					l.league, 
+					this.getDate(scoreboardDate), 
+					scoreboardDate.getTime()
+				);
+
+				for (let j = 0; j < data.events.length; j++) {
+					let e = this.parseEvent(data.events[j]);
+
+					let isFollowedTeam = [e.home.id, e.away.id].some(t => followedTeams.indexOf(t) >= 0);
+
+					if (followOnlyMode) {
+						if (isFollowedTeam) l.games.push(e);
+					} else {
+						l.games.push(e);
+					}
+
+					if (isFollowedTeam && e.live) {
+						l.following.push(`${e.home.teamAbbr}  ${e.home.score} - ${e.away.score}  ${e.away.teamAbbr} [${e.meta}]`);						
+					}
+				} 
+			} catch (error) {}
+
+			if (l.games.length) {
+				events.push(l);
+			}
+		}
+
+		return events;
+	}
+
+	getDate(date) {
+		let parts = this.dateFmt.format(date).split('/');
+		return parts[2] + parts[0] + parts[1];
+	}
+
+	parseEvent(evt) {
+		let home = evt.competitions[0].competitors.find(c => c.homeAway === 'home');
+		let away = evt.competitions[0].competitors.find(c => c.homeAway === 'away');
+
+		let event = {};
+
+		event.live = false;
+		event.home = {};
+		event.home.id = home.id;
+		event.home.team = home.team.shortDisplayName;
+		event.home.teamAbbr = home.team.abbreviation;
+		event.home.score = home.score;
+		event.home.isWinner = 'winner' in home ? home.winner : false;
+		event.home.isLoser = 'winner' in home ? (home.winner === false ? true : false) : false;
+
+		event.away = {};
+		event.away.id = away.id;
+		event.away.team = away.team.shortDisplayName;
+		event.away.teamAbbr = away.team.abbreviation;
+		event.away.score = away.score;
+		event.away.isWinner = 'winner' in away ? away.winner : false;
+		event.away.isLoser = 'winner' in away ?(away.winner === false ? true : false) : false;
+
+		if (evt.status.type.id === STATUS.SCHEDULED) {
+			event.home.score = '';
+			event.away.score = '';
+			event.meta = this.timeFmt.format(new Date(evt.date));
+		} else if (evt.status.type.id === STATUS.FINAL) {
+			event.meta = 'Final';
+		} else if (evt.status.type.id === STATUS.IN_PROGRESS || evt.status.type.id === STATUS.END_PERIOD) {
+			event.live = true;
+			event.meta = evt.status.type.shortDetail;
+		} else {
+			event.home.score = '';
+			event.away.score = '';
+			event.meta = evt.status.type.shortDetail;
+		}
+
+		return event;
+	}
+
+	getEnabledLeagues() {
+		let leagues = [];
+
+		for (let i = 0; i < this._leagues.length; i++) {
+			if (this._settings.get_boolean(this._CONSTANTS.PREF_LEAGUES[this._leagues[i]])) {
+				leagues.push(this._leagues[i]);
+			}
+		}
+
+		return leagues;
+	}
+
+	getFollowedTeams(league) {
+		let teams = [];
+
+		for (let i = 0; i < this._CONSTANTS.SPORTS[league].length; i++) {
+			if (this._settings.get_boolean(this._CONSTANTS.SPORTS[league][i].pref)) {
+				teams.push(this._CONSTANTS.SPORTS[league][i].id.toString());
+			}
+		}
+
+		return teams;
+	}
+
+	isFollowOnlyEnabled() {
+		return this._settings.get_boolean(this._CONSTANTS.PREF_FOLLOWED_ONLY);
+	}
+}
---
+++ colosseum@sereneblue/extension.js
@@ -0,0 +1,215 @@
+const Gio = imports.gi.Gio;
+const Clutter = imports.gi.Clutter;
+const St = imports.gi.St;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const EXTENSION = ExtensionUtils.getCurrentExtension();
+const CONSTANTS = EXTENSION.imports.const;
+const Client = EXTENSION.imports.client;
+
+const Colosseum = new Lang.Class({
+    Name: 'Colosseum',
+    Extends: PanelMenu.Button,
+    _init: function() {
+        this.parent(0, "colosseum", false);
+
+        this._scores = [];
+        this._timeout = null;
+
+        const schemaSource = Gio.SettingsSchemaSource.new_from_directory(
+            EXTENSION.dir.get_child('schemas').get_path(), Gio.SettingsSchemaSource.get_default(), false
+        );
+
+        this._settings = new Gio.Settings({ 
+            settings_schema: schemaSource.lookup("org.gnome.shell.extensions.colosseum", true) 
+        });
+
+        this._client = new Client.ColosseumClient(CONSTANTS, this._settings);
+        this._menuText = new St.Label({
+            text: "Loading...",
+            y_align: Clutter.ActorAlign.CENTER
+        });
+        this.add_child(this._menuText);
+
+        this._update();
+    },
+    _createMenu: function() {
+        let menus = [];
+
+        for (let i = 0; i < this._scores.length; i++) {
+            let baseMenuItem = new PopupMenu.PopupBaseMenuItem({
+                hover: false,
+                activate: false
+            });
+
+            let submenu = new PopupMenu.PopupSubMenuMenuItem(this._scores[i].league);
+            submenu.add_style_class_name('scoreBoardPanel');
+
+            let grid = new Clutter.GridLayout();
+            grid.set_row_homogeneous(false);
+            grid.set_orientation(Clutter.Orientation.VERTICAL);
+
+            let g = new St.Widget({
+                style_class: 'scoreboard',
+                can_focus: false,
+                track_hover: false,
+                reactive: false,
+                layout_manager: grid
+            });
+
+            for (let j = 0; j < ((this._scores[i].games.length * 3) - 1); j++) {
+                grid.insert_row(j);
+                grid.insert_column(j);
+                grid.insert_column(j);
+                grid.insert_column(j);
+            }
+
+            for (let j = 0; j < this._scores[i].games.length; j++) {
+                let homeRow = j * 3;
+                let awayRow = homeRow + 1;
+                let divider_row = (this._scores[i].games.length == 1) ? 0 : (j == this._scores[i].games.length - 1) ? 0 : homeRow + 2;
+
+                let homeSuffix = this._scores[i].games[j].home.isWinner ? '--winner' : this._scores[i].games[j].home.isLoser ? '--loser' : '';
+                let awaySuffix = this._scores[i].games[j].away.isWinner ? '--winner' : this._scores[i].games[j].away.isLoser ? '--loser' : '';
+
+                let homeLabel = new St.Label({ 
+                    text: this._scores[i].games[j].home.team,
+                    style_class: 'team' + homeSuffix,
+                    y_expand: true,
+                    y_align: Clutter.ActorAlign.CENTER 
+                });
+
+                let homeScore = new St.Label({ 
+                    text: this._scores[i].games[j].home.score,
+                    style_class: 'score' + homeSuffix,
+                    y_expand: true,
+                    y_align: Clutter.ActorAlign.CENTER 
+                });
+
+                let gameMeta = new St.Label({ 
+                    text: this._scores[i].games[j].meta,
+                    style_class: 'meta',
+                    y_expand: true,
+                    y_align: Clutter.ActorAlign.CENTER 
+                });
+                
+                grid.attach(homeLabel, 0, homeRow, 1, 1);
+                grid.attach(homeScore, 1, homeRow, 1, 1);
+                grid.attach(gameMeta, 2, homeRow, 1, 1);
+
+                let awayLabel = new St.Label({ 
+                    text: this._scores[i].games[j].away.team,
+                    style_class: 'team' + awaySuffix,
+                    y_expand: true,
+                    y_align: Clutter.ActorAlign.CENTER 
+                });
+
+                let awayScore = new St.Label({ 
+                    text: this._scores[i].games[j].away.score,
+                    style_class: 'score' + awaySuffix,
+                    y_expand: true,
+                    y_align: Clutter.ActorAlign.CENTER 
+                });
+
+                grid.attach(awayLabel, 0, awayRow, 1, 1);
+                grid.attach(awayScore, 1, awayRow, 1, 1);
+                
+                let div = new St.Label({ 
+                    text: "",
+                    style_class: 'divider',
+                    y_expand: true,
+                    y_align: Clutter.ActorAlign.CENTER 
+                });
+
+                if (divider_row) {
+                    grid.attach(div, 0, divider_row, 3, 1);
+                }
+            }
+
+            baseMenuItem.add_actor(g);
+            submenu.menu.addMenuItem(baseMenuItem);
+
+            menus.push(submenu);
+        }
+
+        return menus;
+    },
+    _getUpdateSec: function() {
+        return (this._settings.get_int(CONSTANTS.PREF_UPDATE_FREQ) || 5) * 60;
+    },
+    _update: async function () {
+        await this._loadData();
+
+        let menus = this._createMenu();
+
+        this.menu.removeAll();
+
+        for (let i = 0; i < menus.length; i++) {
+            this.menu.addMenuItem(menus[i]);   
+        }
+
+        this._setTopBarText();
+
+        if (this._timeout) {
+            Mainloop.source_remove(this._timeout);
+            this._timeout = null;
+        }
+
+        this._timeout = Mainloop.timeout_add_seconds(this._getUpdateSec(), Lang.bind(this, this._update));
+    },
+    _loadData: async function() {
+        this._scores = await this._client.getScores();
+    },
+    _setTopBarText: function() {
+        let totalGames = 0;
+        let labelText = "";
+
+        for (let i = 0; i < this._scores.length; i++) {
+            totalGames += this._scores[i].games.length;
+
+            for (let j = 0; j < this._scores[i].following.length; j++) {
+                labelText += (this._scores[i].following[j] + " ");
+            }
+        }
+
+        if (labelText === "") {
+            if (totalGames === 0) {
+                labelText = "No games today";
+            } else {
+                labelText = totalGames + " game(s)";
+            }
+        }
+
+        this._menuText.set_text(labelText);
+    },
+    destroy: function() {
+        this._client.session.abort();
+
+        if (this._timeout) {
+            Mainloop.source_remove(this._timeout);
+            this._timeout = undefined;
+
+            this.menu.removeAll();
+        }
+
+        this.parent();
+    }
+});
+
+let ticker;
+
+function init() {}
+
+function enable() {
+    ticker = new Colosseum;
+    Main.panel.addToStatusArea('colosseum', ticker);
+}
+
+function disable() {
+    ticker.destroy();
+}
